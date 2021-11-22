c08f1b53fa8bba92870935b24c57d6ae3b8fae4a
trancongtruong1234 <ctruong3q3d@gmail.com>
2021-11-22 15:04:28 +0700

    login

--- src/main/java/controller/LoginController.java
+++ src/main/java/controller/LoginController.java
@@ -1,22 +1,17 @@
 package controller;
 import dao.DaoLogin;
-import entity.ListSession;
 import entity.Member;
 
 import java.io.IOException;
-import java.util.ArrayList;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.swing.RepaintManager;
-import javax.swing.text.Document;
 
-import org.apache.tiles.request.Request;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @WebServlet(urlPatterns = {"/login"})
 public class LoginController extends HttpServlet {
@@ -32,45 +27,44 @@ public class LoginController extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		resp.getWriter().append("Served at: ").append(req.getContextPath());
+		req.setAttribute("msg","");
+		RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/login.jsp");
+		dispatcher.forward(req, resp);
 	}
 		
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(req, resp);
+		
+		// doGet(req, resp);
 		resp.setContentType("text/html;charset=UTF-8");
 		
 		String email = req.getParameter("email");
 		String password = req.getParameter("password");
 		try {
 			DaoLogin dao = new DaoLogin();
-			Member member = dao.checkLogin(email, password);
+			Member member = dao.checkLogin(email, password);			
 			if(member != null)
-			{
-				int id = member.getId();
+			{				
 				String username = member.getUserName();
-				
-				HttpSession session = req.getSession();
+				int id = member.getId();
+				HttpSession session = req.getSession(false);
+				// set attribute for new session
 				session.setAttribute("id", id);
 				session.setAttribute("em", email);
 				session.setAttribute("pwd", password);
 				session.setAttribute("us", username);
-				System.out.println(session.getAttribute("id"));
-				/*
-				 * ListSession arrayList = new ListSession(); arrayList.setSession(session);
-				 * System.out.println(arrayList.getSession(0).getAttribute("id"));
-				 */
-				
-				if(session.isNew()) {
+				// set timeout session (seconds)
+				session.setMaxInactiveInterval(60);
+				// login success
 				resp.sendRedirect("home.jsp");
-				}
-				else{
-				resp.sendRedirect("viewContent.tiles");
-				}
+					
+			}else {			
+				// login failed
+				
+				req.setAttribute("msg","Email or password is incorrect!");
+				RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/login.jsp");
+				dispatcher.forward(req, resp);
 			}
-			else resp.sendRedirect("login.jsp");
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
